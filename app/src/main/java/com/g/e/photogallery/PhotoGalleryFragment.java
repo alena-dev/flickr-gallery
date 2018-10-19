0bca90e120e70f4ded5199d1fbc17a414c1fda40
alena <49859442+alena-dev@users.noreply.github.com>
2018-10-19 18:25:30 +0300

    Task-1. Use AsyncTask to run in background thread

--- app/src/main/java/com/g/e/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/g/e/photogallery/PhotoGalleryFragment.java
@@ -1,16 +1,22 @@
 package com.g.e.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.IOException;
+
 public class PhotoGalleryFragment extends Fragment{
+    private static final String TAG = "PhotoGalleryFragment";
+
     private RecyclerView mPhotoRecyclerView;
 
     public static PhotoGalleryFragment createInstance(){
@@ -21,6 +27,7 @@ public class PhotoGalleryFragment extends Fragment{
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Nullable
@@ -34,5 +41,20 @@ public class PhotoGalleryFragment extends Fragment{
                 getActivity(), 3));
 
         return view;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try{
+                String result = new FlickrFetchr()
+                        .getUrlString("https://www.bignerdranch.com");
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException ioe){
+                Log.e(TAG, "Failed to fetch URL: ", ioe);
+            }
+            return null;
+        }
     }
 }

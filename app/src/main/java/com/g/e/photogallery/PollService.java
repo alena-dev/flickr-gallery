3d00ef0afb63fa383d233c831aa0172425c5396d
alena <49859442+alena-dev@users.noreply.github.com>
2019-01-08 16:27:52 +0300

    Task-4. Implement notification

--- app/src/main/java/com/g/e/photogallery/PollService.java
+++ app/src/main/java/com/g/e/photogallery/PollService.java
@@ -2,12 +2,16 @@ package com.g.e.photogallery;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import java.util.List;
@@ -52,24 +56,42 @@ public class PollService extends IntentService{
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
-        if(!isNetworkAvailableAndConnected())return;
+        if (!isNetworkAvailableAndConnected()) return;
 
         String query = QueryPreferences.getStoredQuery(this);
         String lastResultId = QueryPreferences.getLastResultId(this);
         List<GalleryItem> items;
 
-        if(query == null)
+        if (query == null)
             items = new FlickrFetchr().fetchRecentPhotos();
         else
             items = new FlickrFetchr().searchPhotos(query);
 
-        if(items.size() == 0) return;
+        if (items.size() == 0) return;
 
         String resultId = items.get(0).getId();
-        if(resultId.equals(lastResultId))
+        if (resultId.equals(lastResultId)){
             Log.i(TAG, "Got an old result: " + resultId);
-        else
+        }else {
             Log.i(TAG, "Got a new result: " + resultId);
+
+            Resources resources = getResources();
+            Intent intent1 = PhotoGalleryActivity.createIntent(this);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent1, 0);
+
+            Notification notification = new NotificationCompat.Builder(this)
+                    .setTicker(resources.getString(R.string.new_pictures_title))
+                    .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                    .setContentTitle(resources.getString(R.string.new_pictures_title))
+                    .setContentText(resources.getString(R.string.new_pictures_text))
+                    .setContentIntent(pendingIntent)
+                    .build();
+
+            NotificationManagerCompat notificationManager =
+                    NotificationManagerCompat.from(this);
+            notificationManager.notify(0, notification);
+
+        }
 
         QueryPreferences.setLastResultId(this, resultId);
     }

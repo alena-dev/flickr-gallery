00cb9deaf6c44f29e4b24e2ed22de9dbfa63c765
alena <49859442+alena-dev@users.noreply.github.com>
2019-01-14 19:39:45 +0300

    Task-5. Create and register a standalone broadcast receiver

---
+++ app/src/main/java/com/g/e/photogallery/StartupReceiver.java
@@ -0,0 +1,15 @@
+package com.g.e.photogallery;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class StartupReceiver extends BroadcastReceiver{
+    private final String TAG = "StartupReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Received broadcast intent: " + intent.getAction());
+    }
+}
